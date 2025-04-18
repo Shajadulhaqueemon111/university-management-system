d85797a787a0519143b788b36feb7f3a24b3940b
Md Emon <emdshajdulhaque@gmail.com>
2025-04-19 01:15:14 +0600

    setap university management system backend

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    ignores: ["**/node_modules/", ".dist/"],
+    files: ["**/*.{js,mjs,cjs,ts}"],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  tseslint.configs.recommended,
+]);
