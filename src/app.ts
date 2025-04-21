81ffa049bce8dc7ea7a2385e0c1ea37e7a7fb5b6
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-04-21 11:54:35 +0600

    added globalerrorhandeller functionality implement complete

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { StudentRoute } from './app/modules/student/student.route';
 import { UserRouter } from './app/modules/user/user.routes';
+import globalErrorHandeler from './app/middlewares/globalErrorHandeler';
+
 const app: Application = express();
 
 app.use(express.json());
@@ -12,18 +14,11 @@ app.use(cors());
 //application route
 app.use('/api/v1/students', StudentRoute);
 app.use('/api/v1/users', UserRouter);
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 //global error handeller
-app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = err?.statusCode || 500;
-  const message = err?.message || 'Something went wrong!';
+app.use(globalErrorHandeler);
 
-  res.status(statusCode).json({
-    success: false,
-    message,
-    error: err,
-  });
-});
 export default app;
